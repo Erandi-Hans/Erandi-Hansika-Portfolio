756303aa4d74d3c719bb6c40b21821e43f6d2a4e
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-07-20 23:19:02 +0530

    feat:  Add Experiance components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <About />
       <Skills />
+      <Experience />
     </div>
   );
 }
